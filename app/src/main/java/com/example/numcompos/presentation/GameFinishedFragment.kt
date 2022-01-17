4735fb9cd87ee30b69fb4625fb63c260c51eec4c
tonlirise <tonlirise@gmail.com>
2022-01-17 23:10:38 +0700

    Довален механизм Parcelize при передачи данных

--- app/src/main/java/com/example/numcompos/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/numcompos/presentation/GameFinishedFragment.kt
@@ -44,7 +44,9 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun parsParams(){
-        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+        requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
+            gameResult = it
+        }
     }
 
     private fun restartGame(){
@@ -60,7 +62,7 @@ class GameFinishedFragment : Fragment() {
         fun newInstance(gameResult : GameResult): GameFinishedFragment{
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply{
-                    putSerializable(KEY_GAME_RESULT, gameResult)
+                    putParcelable(KEY_GAME_RESULT, gameResult)
                 }
             }
         }
